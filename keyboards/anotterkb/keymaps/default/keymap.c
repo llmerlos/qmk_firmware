e5c225172d905662370905875c2065e0afe55555
llmerlos <llmerlos@outlook.com>
2024-03-12 20:53:43 +0100

    NIRVANA (gaming fix)

--- keyboards/anotterkb/keymaps/default/keymap.c
+++ keyboards/anotterkb/keymaps/default/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //+--------+--------+--------+--------+--------+--------+                 +--------+--------+--------+--------+--------+--------+
    KC_ESC,  KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
 //+--------+--------+--------+--------+--------+--------+                 +--------+--------+--------+--------+--------+--------+
-   KC_B,    KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LGUI,
+   KC_T,    KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LGUI,
 //+--------+--------+--------+--------+--------+--------+                 +--------+--------+--------+--------+--------+--------+
                                        KC_LALT, KC_SPC,  GAMNUM,  LOWER,   KC_BSPC, ALPHAS
 //                                    +--------+--------+--------+--------+--------+--------+
